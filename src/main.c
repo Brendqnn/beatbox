359f68e9767ef800a32f306562061aa8a6bd8788
Brendqnn <bbbird226@gmail.com>
2023-08-19 12:22:17 -0500

    music

--- src/main.c
+++ src/main.c
@@ -1,28 +1,23 @@
 #include <stdio.h>
 #include <raylib.h>
+#include <complex.h>
+#include <math.h>
+#include <stdlib.h>
 
 
 int main(void) {
-    const int win_width = 800;
-    const int win_height = 600;
-
-    InitWindow(win_width, win_height, "BeatBox");
-
-    InitAudioDevice();
-    Sound sound = LoadSound("res/crash.mp3");
-    PlaySound(sound);
-
-    SetSoundVolume(sound, 0.1f);
-    
-
+    InitWindow(800, 800, "BeatBox");
     SetTargetFPS(60);
+    
+    InitAudioDevice();
+    Music sound = LoadMusicStream("res/crash.mp3");
+    PlayMusicStream(sound);
     
     // Main loop
     while (!WindowShouldClose()) {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-                        
-        DrawText("Hello, Raylib!", 10, 10, 20, DARKGRAY);
+        ClearBackground(RED);
+        UpdateMusicStream(sound);
         EndDrawing();
     }
     
