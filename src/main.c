704d0557b0d946c51f01e1568c554c29c09a6ce2
brendan <brendan@debian>
2023-11-05 13:38:17 -0600

    fixed rendering

--- src/main.c
+++ src/main.c
@@ -11,26 +11,25 @@
 
 uint64_t global_frames[2048] = {0};
 size_t global_frames_count = 0;
+ 
+static void fft(float in[], size_t stride, float complex out[], size_t n)
+{
+    assert(n > 0);
 
-
-static void fft(complex double* x, int N) {
-    if (N <= 1) return;
-
-    complex double even[N / 2];
-    complex double odd[N / 2];
-
-    for (int i = 0; i < N / 2; i++) {
-        even[i] = x[i * 2];
-        odd[i] = x[i * 2 + 1];
+    if (n == 1) {
+        out[0] = in[0];
+        return;
     }
 
-    fft(even, N / 2);
-    fft(odd, N / 2);
+    fft(in, stride * 2, out, n / 2);
+    fft(in + stride, stride * 2, out + n / 2, n / 2);
 
-    for (int k = 0; k < N / 2; k++) {
-        complex double T = cexp(-2.0 * I * M_PI * k / N) * odd[k];
-        x[k] = even[k] + T;
-        x[k + N / 2] = even[k] - T;
+    for (size_t k = 0; k < n / 2; ++k) {
+        float t = (float)k / n;
+        float complex v = cexpf(-2.0 * I * PI * t) * out[k + n / 2];
+        float complex e = out[k];
+        out[k] = e + v;
+        out[k + n / 2] = e - v;
     }
 }
 
@@ -40,41 +39,41 @@ void callback(void *buffer_data, unsigned int frames)
         frames = ARRAY_LEN(global_frames);
     }
 
-    complex double complex_data[frames];
+    float audio_data[frames];
     for (size_t i = 0; i < frames; i++) {
-        float sample = ((float *)buffer_data)[i];       
-        complex_data[i] = sample;
+        audio_data[i] = ((float *)buffer_data)[i];
     }
-    
+
     // Apply the Hann window (Hanning window) - https://en.wikipedia.org/wiki/Hann_function
     for (size_t i = 0; i < frames; i++) {
-        float t = (float)i/(frames - 1);
-        float hann = 0.5 - 0.5*cosf(2*PI*t);
-        complex_data[i] *= hann;
+        float t = (float)i / (frames - 1);
+        float hann = 0.5 - 0.5 * cosf(2 * PI * t);
+        audio_data[i] *= hann;
     }
 
-    // Perform FFT
-    fft(complex_data, frames);
+    float complex complex_data[frames];
+    fft(audio_data, 1, complex_data, frames);
 
     for (size_t i = 0; i < frames; i++) {
-        global_frames[i] = creal(complex_data[i]);
+        global_frames[i] = cabs(complex_data[i]);
     }
-    
+
     global_frames_count = frames;
 }
 
 int main(void) {
     InitWindow(1200, 800, "beatbox");
     SetTargetFPS(60);
-    
+
     InitAudioDevice();
-    Music sound = LoadMusicStream("MASTER PAKKU.mp3");
+    Music sound = LoadMusicStream("Beatiful Love.mp3");
     assert(sound.stream.sampleSize == 32);
     assert(sound.stream.channels == 2);
-        
+
     PlayMusicStream(sound);
     SetMusicVolume(sound, 1.5f);
     AttachAudioStreamProcessor(sound.stream, callback);
+
 
     while (!WindowShouldClose()) {
         UpdateMusicStream(sound);
@@ -89,21 +88,18 @@ int main(void) {
         int w = GetRenderWidth();
         int h = GetRenderHeight();
 
-        float x1 = 0;
-        float y1, y2;
-        
+        float height_scale = 12.0f;
+
         BeginDrawing();
         ClearBackground(CLITERAL(Color) {0x18, 0x18, 0x18, 0xFF});
         float cell_width = (float)w/global_frames_count;
+        float cell_height = (float)h/global_frames_count;
         for (size_t i = 0; i < global_frames_count; ++i) {
             complex double data = global_frames[i];
-            y1 = h / 2 - h / 2 * creal(data);
-
-            if (i > 0) {
-                DrawLine(x1, y1, x1 + cell_width, y2, RED);
-            }
-            x1 += cell_width;
-            y2 = y1;
+            float magnitude = cabs(data); // Get the magnitude of the complex value
+            float x = i * cell_width + cell_width/2;
+            float y = h - magnitude * height_scale;
+            DrawRectangle(x, y, cell_width, magnitude * height_scale, RED);
         }
         EndDrawing();
     }
@@ -111,3 +107,4 @@ int main(void) {
 
     return 0;
 }
+
