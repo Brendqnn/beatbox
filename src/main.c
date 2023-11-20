108c219a57f57f78a9aa73452663ddfa695f49d4
brendan <brendan@debian>
2023-11-20 12:41:02 -0600

    fft analyze and render refactor

--- src/main.c
+++ src/main.c
@@ -15,23 +15,20 @@
 
 #define N (1<<13)
 #define ARRAY_LEN(xs) sizeof(xs)/sizeof(xs[0])
+#define MAX_FILEPATH_RECORDED   4096
+#define MAX_FILEPATH_SIZE       2048
+#define SAMPLE_RATE 4800
 
 typedef struct {
     float in_raw[N];
+    float in_win[N];
     float complex out_raw[N];
+    float out_log[N];
     unsigned int frame_count;
 } Track_r;
 
-Track_r *track = NULL;
-
-// Hann window (Hanning window) - https://en.wikipedia.org/wiki/Hann_function
-static inline void hann_window(float *in, size_t n)
-{
-    for (size_t i = 0; i < n; ++i) {
-        float multiplier = 0.5f * (1.0f - cosf(2.0f * PI * i / (n - 1)));
-        in[i] *= multiplier;
-    }
-}
+static Track_r *track = NULL;
+static Music sound = {0};
 
 static inline float amplitude(float complex z)
 {
@@ -62,43 +59,75 @@ static void fft(float in[], size_t stride, float complex out[], size_t n)
     }
 }
 
-static void fft_push(float *buffer, unsigned int frames)
+static size_t fft_analyze(float dt)
 {
-    track->frame_count = frames;
-    memcpy(track->in_raw, buffer, track->frame_count * sizeof(float));
+    // Apply the Hann Window on the Input - https://en.wikipedia.org/wiki/Hann_function
+    for (size_t i = 0; i < N; ++i) {
+        float t = (float)i/(N - 1);
+        float hann = 0.5 - 0.5*cosf(2*PI*t);
+        track->in_win[i] = track->in_raw[i]*hann;
+    }
+    
+    // FFT
+    fft(track->in_win, 1, track->out_raw, N);
 
-    hann_window(track->in_raw, track->frame_count);
-    fft(track->in_raw, 1, track->out_raw, N);
+    // "Squash" into the Logarithmic Scale
+    float step = 1.06;
+    float lowf = 1.0f;
+    size_t m = 0;
+    float max_amp = 1.0f;
+    for (float f = lowf; (size_t) f < N/2; f = ceilf(f*step)) {
+        float f1 = ceilf(f*step);
+        float a = 0.0f;
+        for (size_t q = (size_t) f; q < N/2 && q < (size_t) f1; ++q) {
+            float b = amplitude(track->out_raw[q]);
+            if (b > a) a = b;
+        }
+        if (max_amp < a) max_amp = a;
+        track->out_log[m++] = a;
+    }
+    
+    return m;
+}
+
+static void fft_push(float frame)
+{
+    memmove(track->in_raw, track->in_raw + 1, (N - 1)*sizeof(track->in_raw[0]));
+    track->in_raw[N-1] = frame;
 }
 
 static void callback(void *buffer_data, unsigned int frames)
 {   
-    float *buffer = (float*)buffer_data;
-    fft_push(buffer, frames);
+    // https://cdecl.org/?q=float+%28*fs%29%5B2%5D
+    float (*fs)[2] = buffer_data;
+
+    for (size_t i = 0; i < frames; ++i) {
+        fft_push(fs[i][0]);
+    }
+    float dt = 1.0f/ (float)SAMPLE_RATE;
+
+    // Analyze the audio frames using FFT
+    size_t numValidElements = fft_analyze(dt);
 }
 
-void fft_render(void)
+void fft_render()
 {
     int w = GetRenderWidth();
     int h = GetRenderHeight();
 
-    float scale = 2.0f;
-    float cell_width = (float)w / track->frame_count;
+    float scale = 12.0f;
+    size_t numElements = N/2;
+    float cell_width = (float)w / numElements * 2;
 
-    for (size_t i = 0; i < track->frame_count - 1; i++) {
-        complex float data1 = track->out_raw[i];
-        complex float data2 = track->out_raw[i + 1];
+    for (size_t i = 0; i < numElements; i++) {
+        float value1 = track->out_log[i];
+        float value2 = track->out_log[i + 1];
 
-        float mag1 = cabs(data1);
-        float mag2 = cabs(data2);
-        float amp1 = amplitude(data1);
-        float amp2 = amplitude(data2);
-
-        float hue = (float)i / (float)track->frame_count;
+        float hue = (float)i / (float)numElements;
         Color color = ColorFromHSV(hue * 360, 0.75, 1.0);
 
-        Vector2 start = {i * cell_width, h / 2 - mag1 * amp1/scale};
-        Vector2 end = {(i + 1) * cell_width, h / 2 - mag2 * amp2/scale};
+        Vector2 start = { i * cell_width, h/2 - value1 * scale};
+        Vector2 end = { (i + 1) * cell_width, h/2 - value2 * scale};
 
         DrawLineEx(start, end, cell_width, color);
     }
@@ -108,26 +137,33 @@ static void fft_clean(void)
 {
     memset(track->in_raw, 0, sizeof(track->in_raw));
     memset(track->out_raw, 0, sizeof(track->out_raw));
+
+    UnloadMusicStream(sound);
+    free(track);
 }
 
 int main(void) {
     track = malloc(sizeof(Track_r));
-    SetConfigFlags(FLAG_MSAA_4X_HINT); 
-   
+    
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    
     InitWindow(1200, 800, "beatbox");
+    InitAudioDevice();
     SetTargetFPS(60);
 
-    InitAudioDevice();
-    Music sound = LoadMusicStream("res/Beatiful Love.mp3");
-    assert(sound.stream.sampleSize == 32);
-    assert(sound.stream.channels == 2);
+    int filePathCounter = 0;
+    char *filePaths[MAX_FILEPATH_RECORDED] = {0};
+    const char *filename;
 
-    PlayMusicStream(sound);
-    SetMusicVolume(sound, 1.0f);
-    AttachAudioStreamProcessor(sound.stream, callback);
+    for (int i = 0; i < MAX_FILEPATH_RECORDED; i++) {
+        filePaths[i] = (char *)calloc(MAX_FILEPATH_SIZE, 1);
+    }
 
     while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(CLITERAL(Color) {0x18, 0x18, 0x18, 0xFF});
         UpdateMusicStream(sound);
+
         if (IsKeyPressed(KEY_SPACE)) {
             if (IsMusicStreamPlaying(sound)) {
                 PauseMusicStream(sound);
@@ -135,17 +171,35 @@ int main(void) {
                 ResumeMusicStream(sound);
             }
         }
+        
+        if (IsFileDropped()) {
+            FilePathList droppedFiles = LoadDroppedFiles();
 
-        BeginDrawing();
-        
+            for (int i = 0, offset = filePathCounter; i < (int)droppedFiles.count; i++) {
+                if (filePathCounter < (MAX_FILEPATH_RECORDED - 1)) {
+                    TextCopy(filePaths[offset + i], droppedFiles.paths[i]);
+                    filePathCounter++;
+                }
+                filename = filePaths[i];
+
+                sound = LoadMusicStream(filename);
+                assert(sound.stream.sampleSize == 32);
+                assert(sound.stream.channels == 2);
+
+                PlayMusicStream(sound);
+                SetMusicVolume(sound, 2.0f);
+                AttachAudioStreamProcessor(sound.stream, callback);
+            }
+            UnloadDroppedFiles(droppedFiles);
+        }
+
         fft_render();
-        
+                        
         ClearBackground(CLITERAL(Color) {0x18, 0x18, 0x18, 0xFF});
         EndDrawing();
-    }
 
+    }
     fft_clean();
     CloseWindow();
-
     return 0;
 }
